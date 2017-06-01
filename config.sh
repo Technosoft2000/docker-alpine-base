cc1e0096e09a65c5281e73e101a643816f4a86a8
Technosoft2000 <technosoft2000@gmx.net>
2017-06-01 07:41:23 +0200

    - updated user and group creation/modification

--- config.sh
+++ config.sh
@@ -10,30 +10,24 @@ PGROUP=${PGROUP:=app}
 # (which will be mapped to external group and user, and used to run the process)
 GN=`getent group $PUID | awk -F':' '{print \$1}'`
 if [ $GN ]; then
-  echo "[WARNING] A group with id $PGID exists already [in use by $GN] and won't be created."
+  echo "[WARNING] A group with id $PGID exists already [in use by $GN] and will be modified."
+  groupmod -o -g "$PGID" "$PGROUP"
 else
   echo "[INFO] Create group $PGROUP with id $PGID"
   # create group similar to:
   #  addgroup --gid $PGID $PGROUP
-  # 
-  # changed from:
-  #  addgroup -g $PGID $PGROUP
-  # to:
-  groupmod -o -g "$PGID" "$PGROUP"
+  addgroup -g $PGID $PGROUP
 fi
 
 UN=`grep "^.*:$PUID" /etc/passwd | awk -F':' '{print \$1}'`
 if [ $UN ]; then
-  echo "[WARNING] A user with id $PUID exists already [in use by $UN] and won't be created."
+  echo "[WARNING] A user with id $PUID exists already [in use by $UN] and will be modified."
+  usermod -o -u "$PUID" -g "$PGROUP" "$PUSER"
 else
   echo "[INFO] Create user $PUSER with id $PUID"
   #create user similar to:
   #  adduser --shell /bin/bash --no-create-home --uid $PUID --ingroup $PGROUP --disabled-password --gecos "" $PUSER
-  #
-  # changed from:
-  #  adduser -s /bin/bash -H -u $PUID -G $PGROUP -D -g "" $PUSER
-  # to:
-  usermod -o -u "$PUID" -g "$PGROUP" "$PUSER"
+  adduser -s /bin/bash -H -u $PUID -G $PGROUP -D -g "" $PUSER
 fi
 
 # set the timezone
